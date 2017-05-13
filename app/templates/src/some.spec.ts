925ac5850fa4e0561906690ba6a31eb1ca46722a
Johannes Millan <johannes.millan@gmail.com>
2017-05-13 19:41:54 +0200

    cleanup spec file

--- app/templates/src/some.spec.ts
+++ app/templates/src/some.spec.ts
@@ -1,17 +1,8 @@
 import {Component} from '@angular/core';
-import {DebugElement} from '@angular/core';
 import {async, TestBed} from '@angular/core/testing';
 import {ComponentFixture} from '@angular/core/testing';
-import {By} from '@angular/platform-browser';
 import {userCfg} from './user-cfg';
 
-
-class MockElementRef extends ElementRef {
-  constructor() {
-    super(null);
-    this.nativeElement = {};
-  }
-}
 
 @Component({
   selector: 'test-component',
@@ -31,11 +22,6 @@ describe('', () => {
         TestComponent,
       ],
       providers: [
-        // more providers
-        {
-          provide: ElementRef,
-          useClass: MockElementRef
-        },
         {
           provide: userCfg, useValue: testUserCfg
         },
